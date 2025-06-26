4f15b32258d5ece323ea03e4b72ce201f5a9f2ad
khoward <none>
2025-06-26 10:54:20 -0500

    added comments for clariy

--- 04-storage/s3/s3-static-site/scripts/config.sh
+++ 04-storage/s3/s3-static-site/scripts/config.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-BUCKET_NAME=""
-REGION=""
-PROFILE=""
-FILE_PATH=""
-KMS_KEY_ALIAS=""
+# S3 Bucket orchestration config - fill in before running scripts
+
+BUCKET_NAME=""       # Name of the S3 bucket to create
+REGION=""            # AWS region (e.g., us-east-1)
+PROFILE=""           # AWS CLI profile to use
+FILE_PATH=""         # Path to files to upload
+KMS_KEY_ALIAS=""     # Optional KMS key alias for encryption
 
 ./scripts/orchestrate_bucket.sh create "$BUCKET_NAME" "$REGION" "$PROFILE" "$FILE_PATH" "$KMS_KEY_ALIAS"
